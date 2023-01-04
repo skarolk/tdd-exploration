91856d523c25305f7106f18f0acb76f34b2784d3
skarolk <skarolkiewicz@gmail.com>
2023-01-03 21:50:46 -0600

    test: randomized test run order; verified behavior when modifying an existing exchange rate

--- js/test_money.mjs
+++ js/test_money.mjs
@@ -102,7 +102,7 @@ import {Bank} from "./bank.mjs"
 // }
 
 class MoneyTest {
-    constructor() {
+    setUp() {
         this.bank = new Bank();
         this.bank.addExchangeRate("EUR", "USD", 1.2);
         this.bank.addExchangeRate("USD", "KRW", 1100);
@@ -159,10 +159,13 @@ class MoneyTest {
         assert.throws(() => portfolio.evaluate(this.bank, "Kalganid"), expectedError);
     }
 
-    testConversion() {
+    testConversionWithDifferentRatesBetweenTwoCurrencies() {
         let tenEuros = new Money(10, "EUR");
         assert.deepStrictEqual(
             this.bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+        this.bank.addExchangeRate("EUR", "USD", 1.3);
+        assert.deepStrictEqual(this.bank.convert(tenEuros, "USD"),
+            new Money(13, "USD"));
     }
 
     testConversionWithMissingExchangeRate() {
@@ -175,13 +178,21 @@ class MoneyTest {
             expectedError);
     }
 
+    randomizeTestOrder(testMethods) {
+        for (let i = testMethods.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [testMethods[i], testMethods[j]] = [testMethods[j], testMethods[i]];
+        }
+        return testMethods;
+    }
+
     getAllTestMethods() {
         let moneyPrototype = MoneyTest.prototype;
         let allProps = Object.getOwnPropertyNames(moneyPrototype);
         let testMethods = allProps.filter(p => {
             return typeof moneyPrototype[p] === 'function' && p.startsWith("test");
         });
-        return testMethods;
+        return this.randomizeTestOrder(testMethods);
     }
 
     runAllTests() {
@@ -190,6 +201,7 @@ class MoneyTest {
             console.log("Running: %s()", m);
             let method = Reflect.get(this, m);
             try {
+                this.setUp();
                 Reflect.apply(method, this, []);
             } catch (e) {
                 if (e instanceof assert.AssertionError) {
