59776a86f9dcc16d4df8e1d9846101705cb98305
skarolk <skarolkiewicz@gmail.com>
2023-01-03 21:35:13 -0600

    feat: added Bank; refactored Portfolio to use Bank

--- js/test_money.mjs
+++ js/test_money.mjs
@@ -5,6 +5,7 @@
 import * as assert from "assert";
 import {Money} from "./money.mjs";
 import {Portfolio} from "./portfolio.mjs";
+import {Bank} from "./bank.mjs"
 
 // Old refactored tests
 // let fiver = new Money(5)
@@ -13,21 +14,111 @@ import {Portfolio} from "./portfolio.mjs";
 
 // assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
 
+// class MoneyTest {
+//     testMultiplication() {
+//         let tenEuros = new Money(10, "EUR");
+//         // let twentyEuros = tenEuros.times( 2);
+//         let twentyEuros = new Money(20, "EUR")
+//         // assert.strictEqual(twentyEuros.amount, 20);
+//         // assert.strictEqual(twentyEuros.currency, "EUR");
+//         assert.deepStrictEqual(tenEuros.times(2), twentyEuros)
+//     }
+//
+//     testDivision() {
+//         let originalMoney = new Money(4002, "KRW");
+//         let actualMoneyAfterDivision = originalMoney.divide(4);
+//         let expectedMoneyAfterDivison = new Money(1000.5, "KRW");
+//         assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivison);
+//     }
+//
+//     testAddition() {
+//         let fiveDollars = new Money(5, "USD");
+//         let tenDollars = new Money(10, "USD");
+//         let fifteenDollars = new Money(15, "USD");
+//         let portfolio = new Portfolio();
+//         portfolio.add(fiveDollars, tenDollars);
+//         assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+//     }
+//
+//     testAdditionOfDollarsAndEuros() {
+//         let fiveDollars = new Money(5, "USD")
+//         let tenEuros = new Money(10, "EUR");
+//         let portfolio = new Portfolio();
+//         portfolio.add(fiveDollars, tenEuros);
+//         let expectedValue = new Money(17, "USD")
+//         assert.deepStrictEqual(portfolio.evaluate("USD"), expectedValue)
+//     }
+//
+//     testAdditionOfDollarsAndWons() {
+//         let oneDollar = new Money(1, "USD");
+//         let elevenHundredWon = new Money(1100, "KRW");
+//         let portfolio = new Portfolio();
+//         portfolio.add(oneDollar, elevenHundredWon);
+//         let expectedValue = new Money(2200, "KRW");
+//         assert.deepStrictEqual(portfolio.evaluate("KRW"), expectedValue);
+//     }
+//
+//     testAdditionWithMultipleMissingExchangeRates() {
+//         let oneDollar = new Money(1, "USD");
+//         let oneEuro = new Money(1, "EUR");
+//         let oneWon = new Money(1, "KRW");
+//         let portfolio = new Portfolio();
+//         portfolio.add(oneDollar, oneEuro, oneWon);
+//         let expectedError = new Error(
+//             "Missing exchange rate(s):[USD->Kalganid,EUR->Kalganid,KRW->Kalganid]");
+//         assert.throws(function () {
+//             portfolio.evaluate("Kalganid")
+//         }, expectedError);
+//     }
+//
+//     getAllTestMethods() {
+//         // return ["testMultiplication", 'testDivision', 'testAddition']
+//         let moneyPrototype = MoneyTest.prototype
+//         let allProps = Object.getOwnPropertyNames(moneyPrototype)
+//         return allProps.filter(p => {
+//             return typeof moneyPrototype[p] === 'function' && p.startsWith("test")
+//         })
+//     }
+//
+//     runAllTests() {
+//         // this.testMultiplication()
+//         // this.testDivision()
+//         // this.testAddition()
+//         let testMethods = this.getAllTestMethods()
+//         testMethods.forEach(m => {
+//             console.log("Running: %s()", m)
+//             let method = Reflect.get(this, m)
+//             try {
+//                 Reflect.apply(method, this, [])
+//             } catch (e) {
+//                 if (e instanceof assert.AssertionError) {
+//                     console.log(e)
+//                 } else {
+//                     throw e
+//                 }
+//             }
+//         })
+//     }
+// }
+
 class MoneyTest {
+    constructor() {
+        this.bank = new Bank();
+        this.bank.addExchangeRate("EUR", "USD", 1.2);
+        this.bank.addExchangeRate("USD", "KRW", 1100);
+    }
+
     testMultiplication() {
         let tenEuros = new Money(10, "EUR");
-        // let twentyEuros = tenEuros.times( 2);
-        let twentyEuros = new Money(20, "EUR")
-        // assert.strictEqual(twentyEuros.amount, 20);
-        // assert.strictEqual(twentyEuros.currency, "EUR");
-        assert.deepStrictEqual(tenEuros.times(2), twentyEuros)
+        let twentyEuros = new Money(20, "EUR");
+        assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
     }
 
     testDivision() {
         let originalMoney = new Money(4002, "KRW");
         let actualMoneyAfterDivision = originalMoney.divide(4);
-        let expectedMoneyAfterDivison = new Money(1000.5, "KRW");
-        assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivison);
+        let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+        assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
     }
 
     testAddition() {
@@ -36,16 +127,16 @@ class MoneyTest {
         let fifteenDollars = new Money(15, "USD");
         let portfolio = new Portfolio();
         portfolio.add(fiveDollars, tenDollars);
-        assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+        assert.deepStrictEqual(portfolio.evaluate(this.bank, "USD"), fifteenDollars);
     }
 
     testAdditionOfDollarsAndEuros() {
-        let fiveDollars = new Money(5, "USD")
+        let fiveDollars = new Money(5, "USD");
         let tenEuros = new Money(10, "EUR");
         let portfolio = new Portfolio();
         portfolio.add(fiveDollars, tenEuros);
-        let expectedValue = new Money(17, "USD")
-        assert.deepStrictEqual(portfolio.evaluate("USD"), expectedValue)
+        let expectedValue = new Money(17, "USD");
+        assert.deepStrictEqual(portfolio.evaluate(this.bank, "USD"), expectedValue);
     }
 
     testAdditionOfDollarsAndWons() {
@@ -54,7 +145,7 @@ class MoneyTest {
         let portfolio = new Portfolio();
         portfolio.add(oneDollar, elevenHundredWon);
         let expectedValue = new Money(2200, "KRW");
-        assert.deepStrictEqual(portfolio.evaluate("KRW"), expectedValue);
+        assert.deepStrictEqual(portfolio.evaluate(this.bank, "KRW"), expectedValue);
     }
 
     testAdditionWithMultipleMissingExchangeRates() {
@@ -65,38 +156,49 @@ class MoneyTest {
         portfolio.add(oneDollar, oneEuro, oneWon);
         let expectedError = new Error(
             "Missing exchange rate(s):[USD->Kalganid,EUR->Kalganid,KRW->Kalganid]");
+        assert.throws(() => portfolio.evaluate(this.bank, "Kalganid"), expectedError);
+    }
+
+    testConversion() {
+        let tenEuros = new Money(10, "EUR");
+        assert.deepStrictEqual(
+            this.bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+    }
+
+    testConversionWithMissingExchangeRate() {
+        let bank = new Bank();
+        let tenEuros = new Money(10, "EUR");
+        let expectedError = new Error("EUR->Kalganid");
         assert.throws(function () {
-            portfolio.evaluate("Kalganid")
-        }, expectedError);
+                bank.convert(tenEuros, "Kalganid")
+            },
+            expectedError);
     }
 
     getAllTestMethods() {
-        // return ["testMultiplication", 'testDivision', 'testAddition']
-        let moneyPrototype = MoneyTest.prototype
-        let allProps = Object.getOwnPropertyNames(moneyPrototype)
-        return allProps.filter(p => {
-            return typeof moneyPrototype[p] === 'function' && p.startsWith("test")
-        })
+        let moneyPrototype = MoneyTest.prototype;
+        let allProps = Object.getOwnPropertyNames(moneyPrototype);
+        let testMethods = allProps.filter(p => {
+            return typeof moneyPrototype[p] === 'function' && p.startsWith("test");
+        });
+        return testMethods;
     }
 
     runAllTests() {
-        // this.testMultiplication()
-        // this.testDivision()
-        // this.testAddition()
-        let testMethods = this.getAllTestMethods()
+        let testMethods = this.getAllTestMethods();
         testMethods.forEach(m => {
-            console.log("Running: %s()", m)
-            let method = Reflect.get(this, m)
+            console.log("Running: %s()", m);
+            let method = Reflect.get(this, m);
             try {
-                Reflect.apply(method, this, [])
+                Reflect.apply(method, this, []);
             } catch (e) {
                 if (e instanceof assert.AssertionError) {
-                    console.log(e)
+                    console.log(e);
                 } else {
-                    throw e
+                    throw e;
                 }
             }
-        })
+        });
     }
 }
 
